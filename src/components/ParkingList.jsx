3e13bae8337ee179a9968c6c3adaa26c6c49e922
Amkam <amaduskamara@gmail.com>
2023-07-14 10:00:36 +0000

    Update ParkingList.jsx

--- src/components/ParkingList.jsx
+++ src/components/ParkingList.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Item from "./Item";
 
-const ParkingList = () => {
-  return <div>List</div>;
+const ParkingList = ({ initialItems }) => {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <li>{<Item item={item} />}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ParkingList;
