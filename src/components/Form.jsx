f8f26a9210a32229bd8551be6af074dc398f07d1
Amkam <amaduskamara@gmail.com>
2023-07-14 10:14:19 +0000

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,9 +2,18 @@ import React from "react";
 
 const Form = () => {
   return (
-    <div className="add-form">
+    <form className="add-form">
       <h3>What do you need for your trip? 😀</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item name..." />
+      <button>Add Item</button>
+    </form>
   );
 };
 
