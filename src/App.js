eec639d53da1c43c5bb25018599223ee93a37b57
Amkam <amaduskamara@gmail.com>
2023-07-14 09:59:26 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import Header from "./components/Header";
 import ParkingList from "./components/ParkingList";
 import Stats from "./components/Stats";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
 function App() {
   return (
-    <div>
+    <div className="app">
       <Header />
       <Form />
-      <ParkingList />
+      <ParkingList initialItems={initialItems} />
       <Stats />
     </div>
   );
