5ace76b1d6db05f7599ad1cb8820ac316638573d
Amkam <amaduskamara@gmail.com>
2023-07-14 08:27:01 +0000

    Import components created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import Form from "./components/Form";
+import Header from "./components/Header";
+import ParkingList from "./components/ParkingList";
+import Stats from "./components/Stats";
+
 function App() {
   return (
     <div>
-      <h1>TravelKits</h1>
-      <p></p>
+      <Header />
+      <Form />
+      <ParkingList />
+      <Stats />
     </div>
   );
 }
