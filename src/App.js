7a8f324ecf8e0aea999bf35481e2e65cddc729e3
Amkam <amaduskamara@gmail.com>
2023-07-14 10:14:16 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Stats from "./components/Stats";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Towel", quantity: 12, packed: true },
+  { id: 4, description: "Graduation Shoe", quantity: 12, packed: false },
 ];
 
 function App() {
