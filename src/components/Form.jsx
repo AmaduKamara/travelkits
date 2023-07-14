b7af9cf49ef361571dfb068255fdd110dc8bbafb
Amkam <amaduskamara@gmail.com>
2023-07-14 10:20:45 +0000

    Add event handler on form submission

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 const Form = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <form className="add-form">
       <h3>What do you need for your trip? 😀</h3>
@@ -12,7 +15,7 @@ const Form = () => {
         ))}
       </select>
       <input type="text" placeholder="Item name..." />
-      <button>Add Item</button>
+      <button onClick={handleSubmit}>Add Item</button>
     </form>
   );
 };
