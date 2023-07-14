d00b5408131b792d5f19915fc3c5a43f955286b5
Amkam <amaduskamara@gmail.com>
2023-07-14 08:26:46 +0000

    Create Stats.jsx

---
+++ src/components/Stats.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Stats = () => {
+  return (
+    <footer>
+      <p>You have X items on your list, and you already packed X (X%)</p>
+    </footer>
+  );
+};
+
+export default Stats;
