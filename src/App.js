3bb0a1832f3c8afeb2e08d18d4df8016fb0fd387
Amkam <amaduskamara@gmail.com>
2023-07-16 17:22:55 +0000

    Lifting state up to a single source of truth

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
+import { useState } from "react";
 import Form from "./components/Form";
 import Header from "./components/Header";
 import ParkingList from "./components/ParkingList";
 import Stats from "./components/Stats";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "Towel", quantity: 12, packed: true },
-  { id: 4, description: "Graduation Shoe", quantity: 12, packed: false },
-];
-
 function App() {
+  const [items, setItems] = useState([
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: true },
+    { id: 3, description: "Towel", quantity: 12, packed: true },
+    { id: 4, description: "Graduation Shoe", quantity: 12, packed: false },
+  ]);
+
+  const hancleAddItems = (item) => {
+    setItems((prevItems) => [...prevItems, item]);
+  };
+
   return (
     <div className="app">
       <Header />
-      <Form />
-      <ParkingList initialItems={initialItems} />
+      <Form onAddItems={hancleAddItems} />
+      <ParkingList items={items} />
       <Stats />
     </div>
   );
