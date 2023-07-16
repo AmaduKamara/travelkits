2f76823f0019dead051d2b614a5fe933f5f66d42
Amkam <amaduskamara@gmail.com>
2023-07-16 23:23:02 +0000

    Update Stats.jsx

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 
 const Stats = ({ items }) => {
-  const itemsStats =
-    (100 / items.length) * items.filter((item) => item.packed === true).length;
-  console.log(itemsStats);
+  if (items.length === 0)
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </footer>
+    );
+
+  const itemsCount = items.length;
+  const itemsPacked = items.filter((item) => item.packed).length;
+  const percentages = Math.round((itemsPacked / itemsCount) * 100);
+  // const itemsStats = (100 / items.length) * itemsPacked.length; // itemsStats.toFixed(0)
+
   return (
     <footer className="stats">
-      <p>
-        You have {items.length} items on your list, and you already packed{" "}
-        {items.filter((item) => item.packed === true).length}(
-        {itemsStats.toFixed(0)}% of {items.length} travel items)
-      </p>
+      <em>
+        {percentages === 100
+          ? "You are all set! Ready to go ✈️"
+          : `You have ${itemsCount} items on your list, and you already packed
+        ${itemsPacked}(${percentages}% of ${itemsCount} travel items)`}
+      </em>
     </footer>
   );
 };
