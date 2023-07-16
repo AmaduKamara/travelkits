9376939658ef8203e6a3ed1334d90ec5f71d5aed
Amkam <amaduskamara@gmail.com>
2023-07-16 19:30:32 +0000

    Update ParkingList.jsx

--- src/components/ParkingList.jsx
+++ src/components/ParkingList.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Item from "./Item";
 
-const ParkingList = ({ items }) => {
+const ParkingList = ({ items, onDeleteItem }) => {
   return (
     <div className="list">
       <ul>
+        {items.length === 0 && <h2>No travel item packed yet!</h2>}
         {items.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onDeleteItem={onDeleteItem} />
         ))}
       </ul>
     </div>
