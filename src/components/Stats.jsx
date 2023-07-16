83aa4482bd37945cfe78bd31051174561a83f2c3
Amkam <amaduskamara@gmail.com>
2023-07-16 19:30:35 +0000

    Update Stats.jsx

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
-const Stats = () => {
+const Stats = ({ items }) => {
+  const itemsStats =
+    (100 / items.length) * items.filter((item) => item.packed === true).length;
+  console.log(itemsStats);
   return (
     <footer className="stats">
-      <p>You have X items on your list, and you already packed X (X%)</p>
+      <p>
+        You have {items.length} items on your list, and you already packed{" "}
+        {items.filter((item) => item.packed === true).length}(
+        {itemsStats.toFixed(0)}% of {items.length} travel items)
+      </p>
     </footer>
   );
 };
