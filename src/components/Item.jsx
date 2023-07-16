5e66404b3eb1f6cb65879f6ace8c0ec206f010be
Amkam <amaduskamara@gmail.com>
2023-07-16 20:16:10 +0000

    Update Item.jsx

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
-const Item = ({ item, onDeleteItem }) => {
+const Item = ({ item, onDeleteItem, onToggleItem }) => {
   return (
     <li>
+      <input
+        type="checkbox"
+        checked={item.packed}
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
