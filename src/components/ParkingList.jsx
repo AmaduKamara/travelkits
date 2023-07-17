4191af426f78c7b437470f21d854d48477ca4976
Amkam <amaduskamara@gmail.com>
2023-07-17 09:49:59 +0000

    Add sorting functionality

--- src/components/ParkingList.jsx
+++ src/components/ParkingList.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 const ParkingList = ({ items, onDeleteItem, onToggleItem }) => {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
         {items.length === 0 && <h2>No travel item packed yet!</h2>}
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             key={item.id}
             item={item}
@@ -15,6 +31,14 @@ const ParkingList = ({ items, onDeleteItem, onToggleItem }) => {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by Input Order</option>
+          <option value="description">Sort by Input Description</option>
+          <option value="packed">Sort by Input Packed</option>
+        </select>
+      </div>
     </div>
   );
 };
